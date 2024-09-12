de6537b9e2f495b22bfb6b16baf655196c1138b4
Diego Cardoso <DiegoCardoso@microsoft.com>
2024-09-12 09:50:31 -0300

    v1 combat system

--- internal/battle/battle_test.go
+++ internal/battle/battle_test.go
@@ -10,17 +10,16 @@ import (
 
 func Test_GivenNewBattle_WhenRun2Turns_Then_ShowWinner(t *testing.T) {
 	// arrange
-	expectedNumberOfTurns := 2
+	maxTurns := 2
 	player1, _ := player.New(player.BuildSoldier("Player-1"))
 	player2, _ := player.New(player.BuildSoldier("Player-2"))
 	players := []player.Player{*player1, *player2}
 	dice := dice.New(dice.BuildDefault())
-	battle := New(players, dice, dice, expectedNumberOfTurns)
+	battle := New(players, dice, dice, maxTurns)
 	//act
 	battle.RunTurns()
 	//assert
-	assert.Equal(t, expectedNumberOfTurns, battle.TotalNumberOfTurns())
-	assert.Greater(t, battle.Winner.Health.Current, battle.Looser.Health.Current)
+	assert.Equal(t, maxTurns, battle.TotalNumberOfTurns())
 	assert.True(t, battle.Players[0].IsAlive())
 	assert.True(t, battle.Players[1].IsAlive())
 	assert.Equal(t, battle.Status, Unfinished)
@@ -28,7 +27,7 @@ func Test_GivenNewBattle_WhenRun2Turns_Then_ShowWinner(t *testing.T) {
 }
 
 func Test_WhenRun2Turns_Then_ShowDraw(t *testing.T) {
-	// arrange
+	//arrange
 	expectedNumberOfTurns := 2
 	player1, _ := player.New(player.BuildSoldier("Player-1"))
 	player2, _ := player.New(player.BuildSoldier("Player-2"))
@@ -135,4 +134,51 @@ func Test_WhenRun3Turns_Knight_Archer_Then_ShowWinner(t *testing.T) {
 	assert.Greater(t, battle.Winner.Health.Current, 0)
 	assert.True(t, battle.Winner.IsAlive())
 	assert.Equal(t, battle.Status, Unfinished)
+}
+
+func Test_GivenNewBattle_WhenRunTurns_Then_ValidateResult(t *testing.T) {
+
+	turns := []int{1, 2, 3, 4, 10, 20, 30, 50, 100}
+
+	for i := 0; i < len(turns); i++ {
+		// arrange
+		maxTurns := turns[i]
+		player1, _ := player.New()
+		player2, _ := player.New()
+		players := []player.Player{*player1, *player2}
+		normalDice := dice.New(dice.BuildDefault())
+		magicDice := dice.New(dice.BuildDefaultMagic())
+		battle := New(players, normalDice, magicDice, maxTurns)
+		//act
+		battle.RunTurns()
+		//assert
+		assert.Greater(t, battle.TotalNumberOfTurns(), 0)
+		assert.LessOrEqual(t, battle.TotalNumberOfTurns(), maxTurns)
+
+		if battle.Status == Draw {
+			assert.Nil(t, battle.Winner)
+			assert.Nil(t, battle.Looser)
+			assert.Equal(t, battle.Status, Draw)
+		}
+
+		if battle.Status == Unfinished {
+			assert.NotNil(t, battle.Winner)
+			assert.NotNil(t, battle.Looser)
+			assert.Greater(t, battle.Winner.Health.Current, 0)
+			assert.True(t, battle.Winner.IsAlive())
+			assert.Greater(t, battle.Looser.Health.Current, 0)
+			assert.True(t, battle.Looser.IsAlive())
+			assert.Equal(t, battle.Status, Unfinished)
+		}
+
+		if battle.Status == Finished {
+			assert.NotNil(t, battle.Winner)
+			assert.NotNil(t, battle.Looser)
+			assert.Greater(t, battle.Winner.Health.Current, 0)
+			assert.True(t, battle.Winner.IsAlive())
+			assert.Equal(t, battle.Looser.Health.Current, 0)
+			assert.False(t, battle.Looser.IsAlive())
+			assert.Equal(t, battle.Status, Finished)
+		}
+	}
 }
